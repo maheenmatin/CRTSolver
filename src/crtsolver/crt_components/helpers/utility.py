93f2f513f563a6707803c8ed94af3b3a83644d14
maheenmatin <maheenmatin@gmail.com>
2025-11-30 03:16:44 +0000

    Solvers working

---
+++ src/crtsolver/crt_components/helpers/utility.py
@@ -0,0 +1,72 @@
+import cvc5
+from cvc5 import Kind
+from crtsolver.crt_components.errors import error
+
+class Utility:
+    def __init__(self, API, terms, primes, bitwidth, main):
+        self.API = API
+        self.terms = terms
+        self.primes = primes
+        self.bitwidth = bitwidth
+        self.main = main
+        self.operator_mapping = {
+            "=": Kind.EQUAL,
+            "+": Kind.ADD,
+            "-": Kind.SUB,
+            "*": Kind.MULT,
+            ">": Kind.GT,
+            "<": Kind.LT,
+            ">=": Kind.GEQ,
+            "<=": Kind.LEQ
+        }
+        self.bv_operator_mapping = {
+            "=": Kind.EQUAL,
+            "+": Kind.BITVECTOR_ADD,
+            "-": Kind.BITVECTOR_SUB,
+            "*": Kind.BITVECTOR_MULT,
+            ">": Kind.BITVECTOR_UGT, # unsigned greater than
+            "<": Kind.BITVECTOR_ULT,
+            ">=": Kind.BITVECTOR_UGE,
+            "<=": Kind.BITVECTOR_ULE
+        }
+
+    def create_term(self, operator, operands):
+        return self.API.solver.mkTerm(self.operator_mapping[operator], *operands)
+    
+    def create_mod_term(self, operator, operands):
+        return self.API.mod_solver.mkTerm(self.operator_mapping[operator], *operands)
+    
+    def create_bv_mod_term(self, operator, operands):
+        return self.API.mod_solver.mkTerm(self.bv_operator_mapping[operator], *operands)
+        
+    def handle_integer(self, num):  
+        try: 
+            if num in self.terms.ints: # if num already exists
+                return self.terms.ints[num]
+            else:     
+                num_term = self.API.tm.mkInteger(int(num)) # convert string to integer
+                self.terms.ints[num] = num_term # add to dictionary
+                return num_term
+        except OverflowError:
+            raise error.AbortFileException(num)
+        
+    def handle_bv_mod_const(self, const):
+        # Return equivalent constant for mod p
+        mod_name = f"{const}_mod_{self.primes.prime}"
+
+        if mod_name in self.terms.bv_mod_vars: # if equivalent constant already exists
+            return self.terms.bv_mod_vars[mod_name]
+        else:
+            #print(f"Making constant {mod_name}")
+            new_const = self.API.tm.mkConst(self.bitwidth.n_sort, mod_name)
+            self.terms.bv_mod_vars[mod_name] = new_const
+            return new_const
+        
+    def handle_bv_integer(self, num):
+        if num in self.terms.bv_ints:
+            return self.terms.bv_ints[num]
+        else:
+            # Represent as bitvector of width n     
+            num_term = self.API.tm.mkBitVector(self.bitwidth.n, int(num))
+            self.terms.bv_ints[num] = num_term
+            return num_term
---
+++ src/crtsolver/solvers/z3_solver.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from z3 import *
+import time
+from crtsolver.input_output import reader, writer
+
+# NOTE: Inspired by code and instructions from the following sources:
+# NOTE: https://ericpony.github.io/z3py-tutorial/guide-examples.htm
+# NOTE: https://z3prover.github.io/papers/programmingz3.html
+class Z3Solver:
+    def __init__(self, time_limit, solver_name):
+        # Set root directory for robust file paths
+        # CRTSolver -> src -> solvers -> z3_solver.py
+        # z3_solver.py = file, solvers = parents[0], crtsolver = parents[1],
+        # src = parents[2], CRTSolver = parents[3]
+        self.ROOT = Path(__file__).resolve().parents[3]
+
+        # Set absolute paths from root directory
+        self.TESTS = self.ROOT / "tests"
+        self.RESULTS = self.ROOT / "results"
+
+        self.time_limit = time_limit
+        self.solver_name = solver_name
+        self.writer = writer.Writer(self.RESULTS, self.solver_name)
+        
+    def reinit(self):
+        # Create solver
+        self.solver = Solver()
+        
+        # Set solver options
+        self.solver.set(unsat_core=True)
+        self.solver.set(timeout=int(self.time_limit))
+
+        self.start_time = time.time()
+        self.sat_model = [] # if SAT, stores satisfying values
+
+    def get_solver_name(self):
+        return self.solver_name
+
+    def execute(self):
+        for file in reader.get_sorted_files(self.TESTS):
+            if file.is_file():
+                # Reinitialize data for new file
+                self.reinit()
+                print(f"Reading file: {file}")
+
+                #with file.open("r") as input:
+                    #input_code = input.read()
+
+                self.solver.from_file(str(file)) # from_file expects string, not Path
+
+                # Check satisfiability
+                result = self.solver.check()
+                if result == sat:
+                    model = self.solver.model()
+                    # Get all declared variable names and terms
+                    for decl in model.decls():
+                        name = decl.name() # constant name
+                        value = model[decl] # constant value
+                        self.sat_model.append([name, value])
+                elif result == unsat:
+                    self.sat_model.append(["UNSAT"])
+                elif result == unknown:
+                    self.sat_model.append(["UNKNOWN (TIMEOUT)"])
+
+                print()
+                self.writer.store_result(file, self.start_time, self.sat_model)
+        self.writer.write()
+
+def main():
+    z3_solver = Z3Solver("100", "Z3")
+    z3_solver.execute()
+
+if __name__ == "__main__":
+    main()
